f4e1fa07542d783560bf75cd6375b2917b84a8ce
nioyh <jen00913@gmail.com>
2025-07-11 13:39:10 +0900

    Update ContentView.swift
    로그아웃 시 로그인 페이지 안뜨는 오류 수정

--- MediMate/MediMate/App/ContentView.swift
+++ MediMate/MediMate/App/ContentView.swift
@@ -1,43 +1,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("isLoggedIn") var isLoggedIn = false
+
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("홈")
-                }
+        if isLoggedIn {
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("홈")
+                    }
 
-            ReminderTabView()
-                .tabItem {
-                    Image(systemName: "alarm.fill")
-                    Text("알림")
-                }
-            
+                ReminderTabView()
+                    .tabItem {
+                        Image(systemName: "alarm.fill")
+                        Text("알림")
+                    }
 
-            AnalyzeView()
-                .tabItem {
-                    Image(systemName: "doc.text.magnifyingglass")
-                    Text("분석")
-                }
+                AnalyzeView()
+                    .tabItem {
+                        Image(systemName: "doc.text.magnifyingglass")
+                        Text("분석")
+                    }
 
-            ConsultView()
-                .tabItem {
-                    Image(systemName: "message.fill")
-                    Text("상담")
-                }
+                ConsultView()
+                    .tabItem {
+                        Image(systemName: "message.fill")
+                        Text("상담")
+                    }
 
-           MyPage()
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("마이페이지")
-                }
-
+                MyPage()
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("마이페이지")
+                    }
+            }
+        } else {
+            LoginView() 
         }
     }
-}
-
-#Preview {
-    ContentView()
 }
