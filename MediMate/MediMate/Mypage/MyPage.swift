005faf70b66a6d74c89e7533ea6f310b1e954164
YOONKYEONG07 <cecilia86494026@gmail.com>
2025-07-11 12:46:27 +0900

    Update MyPage.swift

--- MediMate/MediMate/Mypage/MyPage.swift
+++ MediMate/MediMate/Mypage/MyPage.swift
@@ -139,17 +139,3 @@ struct MyPage: View {
         }
     }
 }
-
-struct MyInfoRow: View {
-    var title: String
-    var value: String
-
-    var body: some View {
-        HStack {
-            Text(title)
-            Spacer()
-            Text(value)
-                .foregroundColor(.secondary)
-        }
-    }
-}
