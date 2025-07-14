be8070c4a65430b29acb9252916c0fa3055cd7bc
seoyeon0905 <ssseoyeon95@naver.com>
2025-07-14 23:30:58 +0900

    분석페이지의 약이름 옆에 하트누르면 마이페이지의 약 즐겨찾기 리스트에 추가되는거 구현

--- MediMate/MediMate/Mypage/FavoriteDrugsView.swift
+++ MediMate/MediMate/Mypage/FavoriteDrugsView.swift
@@ -1,15 +1,33 @@
-//
-//  FavoriteDrugsView.swift
-//  MediMate
-//
-//  Created by 이효인 on 7/14/25.
-//
-
 import SwiftUI
 
 struct FavoriteDrugsView: View {
+    @State private var favoriteMeds: [String] = []
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                if favoriteMeds.isEmpty {
+                    Text("즐겨찾기한 약이 없습니다.")
+                        .foregroundColor(.gray)
+                        .italic()
+                } else {
+                    ForEach(favoriteMeds, id: \.self) { med in
+                        NavigationLink(destination: MedicationDetailView(medName: med)) {
+                            Text(med)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("즐겨찾는 약")
+            .onAppear {
+                loadFavorites()
+            }
+        }
+    }
+
+    // MARK: - UserDefaults에서 즐겨찾기 불러오기
+    func loadFavorites() {
+        favoriteMeds = UserDefaults.standard.stringArray(forKey: "favoriteMeds") ?? []
     }
 }
 
