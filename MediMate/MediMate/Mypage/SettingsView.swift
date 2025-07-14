31ccb7cf5cdb6310b6661176c05554e14b837dc4
hyo <jen00913@naver.com>
2025-07-14 17:15:55 +0900

    마이페이지 즐겨찾는 약 버튼 추가 , 환경설정 버튼 추가

---
+++ MediMate/MediMate/Mypage/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MediMate
+//
+//  Created by 이효인 on 7/14/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
