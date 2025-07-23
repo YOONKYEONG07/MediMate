cf4d0cd36d17eed5ced390359ff273b7d1033536
YOONKYEONG07 <cecilia86494026@gmail.com>
2025-07-23 16:26:24 +0900

    Delete MediMate/Alarm directory

--- MediMate/Alarm/MedicationReminder.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct MedicationReminder: Identifiable, Codable {
-    let id: String  // 알림 ID (UUID로 생성)
-    let name: String
-    let hour: Int
-    let minute: Int
-    var days: [String]
-}
