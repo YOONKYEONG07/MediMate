3f08a731ae65440124d251bfa616a60aac33dd4c
hyo <jen00913@naver.com>
2025-07-29 17:46:40 +0900

    MediMateApp.swift 파일 이전 커밋으로 복원

--- MediMate/MediMate/App/MediMateApp.swift
+++ MediMate/MediMate/App/MediMateApp.swift
@@ -3,26 +3,20 @@ import Firebase
 
 @main
 struct MediMate: App {
-    @AppStorage("isDarkMode") private var isDarkMode = false
-    @AppStorage("isLoggedIn") private var isLoggedIn = false  // 🔹 자동 로그인 상태 저장 변수 추가
+    @AppStorage("isDarkMode") private var isDarkMode = false  // ✅ 추가
 
     init() {
         FirebaseApp.configure()
         NotificationManager.instance.requestAuthorization()
+        
         NotificationManager.instance.registerNotificationActions()
-        UNUserNotificationCenter.current().delegate = NotificationManager.instance
+           UNUserNotificationCenter.current().delegate = NotificationManager.instance
     }
 
     var body: some Scene {
         WindowGroup {
-            if isLoggedIn {
-                MyPage()
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
-            } else {
-                LoginView()
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
-            }
+            ContentView()
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
- }
-   
+}
