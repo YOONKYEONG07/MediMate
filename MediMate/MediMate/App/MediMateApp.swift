624764105fbb6062f20db3686ca1c34bbd3c0dca
hyo <jen00913@naver.com>
2025-07-29 17:30:17 +0900

    firebase 연동 및 로그인 상태 유지(자동로그인) 기능 추가

--- MediMate/MediMate/App/MediMateApp.swift
+++ MediMate/MediMate/App/MediMateApp.swift
@@ -3,20 +3,26 @@ import Firebase
 
 @main
 struct MediMate: App {
-    @AppStorage("isDarkMode") private var isDarkMode = false  // ✅ 추가
+    @AppStorage("isDarkMode") private var isDarkMode = false
+    @AppStorage("isLoggedIn") private var isLoggedIn = false  // 🔹 자동 로그인 상태 저장 변수 추가
 
     init() {
         FirebaseApp.configure()
         NotificationManager.instance.requestAuthorization()
-        
         NotificationManager.instance.registerNotificationActions()
-           UNUserNotificationCenter.current().delegate = NotificationManager.instance
+        UNUserNotificationCenter.current().delegate = NotificationManager.instance
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .preferredColorScheme(isDarkMode ? .dark : .light)
+            if isLoggedIn {
+                MyPage()
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+            } else {
+                LoginView()
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+            }
         }
     }
-}
+ }
+   
